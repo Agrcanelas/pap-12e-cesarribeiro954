61cd2616acbae64be20ef058c249d89b40f3b303
César Ribeiro <a10694@agrcanelas.com>
2025-11-17 17:48:17 +0000

    corrigir caminhos index.php

--- includes/header.php
+++ includes/header.php
@@ -2,9 +2,9 @@
 <header>
   <div class="logo">Ecopeças</div>
   <nav class="menu">
-    <a href="index.php"><i class="fa fa-home"></i> Home</a>
-    <a href="cart.php"><i class="fa fa-shopping-cart"></i> Carrinho</a>
-    <a href="finalizar_compra.php"><i class="fa fa-credit-card"></i> Finalizar Compra</a>
+    <a href="./index.php"><i class="fa fa-home"></i> Home</a>
+    <a href="./cart.php"><i class="fa fa-shopping-cart"></i> Carrinho</a>
+    <a href="./finalizar_compra.php"><i class="fa fa-credit-card"></i> Finalizar Compra</a>
   </nav>
   <div class="user-menu">
     <a href="auth/login.php"><i class="fa fa-user"></i> Login</a>
